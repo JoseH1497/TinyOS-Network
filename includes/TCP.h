ef98c672bc51e004a3a85b065a3929afe279ebdf
elkomanderJOZZI <33468911+elkomanderJOZZI@users.noreply.github.com>
2017-12-04 14:58:20 -0800

    Add files via upload

---
+++ includes/TCP.h
@@ -0,0 +1,96 @@
+#ifndef TCP_H
+#define TCP_H
+
+#include "packet.h"
+
+enum{
+
+    BUFFERSIZE = 128,
+    SERVERPORT =255,
+    LISTENING = 700,
+    AVAILABLE = 701,
+    SYN_SENT = 702,
+    SYN_RCVD = 703,
+    ESTABLISHED = 704,
+    AWAITING = 705,
+    FIN_WAIT_1 = 706,
+    FIN_WAIT_2 = 719,
+    CLOSED = 707,
+    BUFFER = PACKET_MAX_PAYLOAD_SIZE - 8,
+    sendServerData = 888,
+    clientSentData = 999,
+    //Flags
+    URG = 100,
+    ACK = 200,
+    PSH = 300,
+    SYN_CLIENT = 400,
+    SYN_SERVER = 401, //differentiate b/w server and client SYN in 3 way handshake
+    FIN = 500,
+    RST = 600,
+    SYN = 777,
+
+};
+
+
+typedef nx_struct TCP{
+        nx_uint8_t srcPort;
+        nx_uint8_t destPort;
+        nx_uint16_t seqNum;
+        nx_uint8_t ACK;
+        nx_uint16_t flag;
+        nx_uint8_t AdWindow;
+        nx_uint8_t dataLength;
+        nx_uint8_t data[PACKET_MAX_PAYLOAD_SIZE -8];
+        nx_uint8_t doneSending;
+        
+
+}TCP;
+
+
+
+
+typedef struct socket_t{
+        uint8_t srcPort;
+        char sentData[128]; //buffer size = 128
+        
+        uint8_t srcAddr;
+        char recievedData[128];
+        uint8_t destPort;
+        uint8_t destAddr;
+        uint8_t connectionState; //state
+
+        
+        
+        
+
+
+}socket_t;
+
+
+
+typedef struct Port{
+        bool open;
+        uint8_t destAddr;
+        int destPort;
+        int portNumber;
+        uint16_t lastSeq;
+        uint16_t nextSeq;
+        int srcPort;
+        uint16_t state;
+        char recievedData[128];
+        char sentData[128];
+        uint16_t sizeofPayload;
+
+
+}Port;
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ includes/routingTable.h
@@ -0,0 +1,9 @@
+#ifndef ROUTINGTABLE_H
+#define ROUTINGTABLE_H
+
+typedef struct routingTable{
+    uint8_t distance;
+    uint8_t nextHop;
+}routingTable;
+
+#endif /* ROUTINGTABLE_H */
