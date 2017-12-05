228a9a1c4dfab433e177a728b5d3dc94dace248b
elkomanderJOZZI <33468911+elkomanderJOZZI@users.noreply.github.com>
2017-12-05 06:17:00 -0800

    Update TestSim.py

--- TestSim.py
+++ TestSim.py
@@ -187,7 +187,7 @@ def main():
     s.whisper(1,"atrebic ", "HELLO!\r\n");
     #s.whisper(1,3, "HI!\r\n");
     s.runTime(80);
-    s.listusr(3, 1, "listusr\r\n");
+    s.listusr(2, 1, "listusr\r\n");
     s.runTime(80);
     #s.testServer(7);
     #s.runTime(20);
